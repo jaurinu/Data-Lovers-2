ab5bba4a48c054bdd9aee57237e1eb7f48ed73bb
jaurinu <jaurinu@gmail.com>
2019-02-26 07:33:57 -0600

    Se sube proyecto Data Lovers con data injuries

---
+++ src/data.js
@@ -0,0 +1,41 @@
+window.injuries = {
+
+  justFiveYears: (responseJson)=>{
+    console.log(responseJson)
+  const minimumYear=('2011-01-04')
+  const maximumYear=('2016-01-04')
+  const justFiveYears=responseJson.filter(element=>(element.Year)>=(minimumYear)&&element.Year<(maximumYear))
+  console.log(justFiveYears);
+  return justFiveYears;
+  },
+
+  indicatorInjuries: (justFiveYears)=>{
+    
+    const newArrayInjuries=[];  
+        justFiveYears.forEach(element => {
+          let motorcyclists= element.Total_Injured_Persons_Motorcyclists;
+          let pedalcyclists= element.Total_Injured_Persons_Pedalcyclists;
+          let carOccupant= element.Total_Injured_Persons_Passenger_Or_Occupant;
+          let pedestrians= element.Total_Injured_Persons_Pedestrians;
+          let year=element.Year;
+          newArrayInjuries.push({year, motorcyclists, pedalcyclists, carOccupant, pedestrians});
+          
+        });
+       
+        return newArrayInjuries;
+      },
+  
+      // sortTotalInjuries: (sortTotalInjuriesResult) => {
+      //   const orderInjuries = newArrayInjuries
+  
+      // }
+    
+     
+
+
+
+}
+
+
+
+
---
+++ src/main.js
@@ -0,0 +1,158 @@
+//Constantes de páginas//
+const initialPage = document.getElementById('initial-page');
+const motoPage = document.getElementById('moto-page');
+const bicyclePage = document.getElementById('bicycle-page');
+const carPage = document.getElementById('car-page');
+const pedestrianPage = document.getElementById('pedestrian-page');
+const yearPage = document.getElementById('year-page');
+const allYearPage = document.getElementById('all-year-page');
+
+//Constantes botones página inicial//
+const motorcyclists = document.getElementById('motorcyclists');
+const pedalcyclists = document.getElementById('pedalcyclists');
+const carOccupant = document.getElementById('carOccupant');
+const pedestrians = document.getElementById('pedestrians');
+const oneYearButton = document.getElementById('one-year');
+const allYearButton = document.getElementById('all-year');
+const hexagonButton = document.getElementsByClassName('hexagon-button');
+const sortButton = document.getElementById('sort-button');
+
+//Constante fetch data//
+const injuriesData='./data/injuries/injuries.json';
+
+//Constantes funciones imprimir//
+const printData = document.getElementById('print-data');
+const printMotorcyclists=document.getElementById('print-motorcyclists');
+const printPedalcyclists=document.getElementById('print-pedalcyclists');
+const printCarOccupant=document.getElementById('print-car-occupant');
+const printPedestrians=document.getElementById('print-pedestrians');
+const averageMotoInjuries=document.getElementById('average-moto-injuries');
+const averageCyclistInjuries=document.getElementById('average-cyclist-injuries');
+const averageCarInjuries=document.getElementById('average-car-injuries');
+const averagePedestrianInjuries=document.getElementById('average-pedestrian-injuries');
+
+//Eventos de páginas//
+motorcyclists.addEventListener('click', ()=>{
+    initialPage.classList.add('hidePage');
+    motoPage.classList.remove('hidePage');
+});
+
+pedalcyclists.addEventListener('click', ()=>{
+    initialPage.classList.add('hidePage');
+    bicyclePage.classList.remove('hidePage');
+});
+
+carOccupant.addEventListener('click', ()=>{
+    initialPage.classList.add('hidePage');
+    carPage.classList.remove('hidePage');
+});
+
+pedestrians.addEventListener('click', ()=>{
+    initialPage.classList.add('hidePage');
+    pedestrianPage.classList.remove('hidePage');
+});
+
+// oneYearButton.addEventListener('click', ()=>{
+//     initialPage.classList.add('hidePage');
+//     yearPage.classList.remove('hidePage');
+// });
+
+// allYearButton.addEventListener('click', ()=>{
+//     initialPage.classList.add('hidePage');
+//     allYearPage.classList.remove('hidePage');
+// });
+
+//-----------------------Fetch-------------//
+fetch(injuriesData)
+.then((response) => response.json())
+.then(responseJson =>window.injuries.justFiveYears(responseJson))
+.then(justFiveYears => window.injuries.indicatorInjuries(justFiveYears))
+.then(newArrayInjuriesResp => printResult(newArrayInjuriesResp))
+// .then(sortTotalInjuriesResult =>{
+//     sortButton.addEventListener('click', ()=>{
+//         window.injuries.sortTotalInjuries(sortTotalInjuriesResult)
+//     })
+// })
+
+.catch((error)=>{
+    console.info('hubo un problema '+error.message);
+});
+
+//---------------------imprime-------------------------//
+let printResult=(newArrayInjuriesResp) =>{
+    console.log(newArrayInjuriesResp)
+
+    //--------------Conocer el botón seleccionado----------//
+        for (let i=0; i<hexagonButton.length; i++){
+            hexagonButton[i].addEventListener('click',()=>{
+                let chosenButton=hexagonButton[i].id;
+                console.log(chosenButton)
+    //--------------Imprime los elementos seleccionados----------//
+                 let sumInjuries = 0;
+                 const motoArray = [];
+                 const cyclistArray = [];
+                 const carArray = [];
+                 const pedestrianArray = [];
+                 newArrayInjuriesResp.forEach(element => {
+                    
+                    const newYear=new Date(element.year).getFullYear()
+       
+                    if (chosenButton ==='motorcyclists'){
+                        sumInjuries += (element.motorcyclists);
+                        const printP=`<p class='tabla'> ${newYear} - <span>${element.motorcyclists}</span> </p>`
+                        printMotorcyclists.insertAdjacentHTML('beforeend', printP);
+                        motoArray.push([newYear, element.motorcyclists])
+                       
+                    }else if (chosenButton==='pedalcyclists'){
+                        sumInjuries += (element.pedalcyclists);
+                        const printP=`<p class='tabla'> ${newYear} - <span>${element.pedalcyclists}</span> </p>`
+                        printPedalcyclists.insertAdjacentHTML('beforeend', printP);
+                        cyclistArray.push([newYear, element.pedalcyclists])
+                      
+                    }else if (chosenButton==='carOccupant'){
+                        sumInjuries += (element.carOccupant);
+                        const printP=`<p class='tabla'> ${newYear} - <span>${element.carOccupant}</span> </p>`
+                        printCarOccupant.insertAdjacentHTML('beforeend', printP);
+                        carArray.push([newYear, element.carOccupant])
+                      
+                    }else if (chosenButton==='pedestrians'){
+                        sumInjuries += (element.pedestrians);
+                        const printP=`<p class='tabla'> ${newYear} - <span>${element.pedestrians}</span> </p>`
+                        printPedestrians.insertAdjacentHTML('beforeend', printP);
+                        pedestrianArray.push([newYear, element.pedestrians])
+                    }
+                });
+    //--------------Saca promedio de heridos en cinco años-----------------//
+                const average = sumInjuries / newArrayInjuriesResp.length; 
+                console.log(average)
+                averageMotoInjuries.innerHTML = average; 
+                averageCyclistInjuries.innerHTML = average; 
+                averageCarInjuries.innerHTML = average.toFixed(0); 
+                averagePedestrianInjuries.innerHTML = average; 
+                console.log(motoArray)
+                console.log(cyclistArray)
+                console.log(carArray)
+                console.log(pedestrianArray)
+            })
+        }   
+      return newArrayInjuriesResp
+        }
+        // motoArray.sort
+        // sortButton.addEventListener('click', ()=>{
+
+        //     motoArray.sort()
+
+        //     console.log('funciono')
+        //     // window.injuries.sortTotalInjuries(sortTotalInjuriesResult)
+        // })
+        // sortByName: (data) => {
+        //     const orderPokemonName = data.sort((a, b) => {
+        //       if (a.name > b.name) {
+        //         return 1;
+        //       } else {
+        //         return -1;
+        //       }
+        //     })
+        //     return (orderPokemonName)
+        //   }
+        
